e1521acd7a336c9398da3328a8a6ec31b7753635
Adriano Saadeh <adrianosaadeh@gmail.com>
2024-12-03 20:19:09 -0300

    first test using docker

---
+++ web/cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,26 @@
+describe('login spec', () => {
+  it('Deve realizar o pre-cadastro do cliente com sucesso', () => {
+    cy.visit('/')
+
+    cy.get('header nav a[href="pre-cadastro"]')
+      .click()
+
+    cy.get('form h2')
+      .should('be.visible')
+      .and('have.text', 'Seus dados')
+
+    cy.get('input[name="nome"]').type('Adriano QA')
+    cy.get('input[name="email"]').type('adrianoqa@gmail.com')
+
+    cy.contains('button[type="submit"]', 'Continuar').click()
+
+    cy.get('.user-name')
+      .should('be.visible')
+      .and('have.text', 'Adriano QA')
+
+    cy.get('.user-email')
+      .should('be.visible')
+      .and('have.text', 'adrianoqa@gmail.com')
+
+  })
+})
