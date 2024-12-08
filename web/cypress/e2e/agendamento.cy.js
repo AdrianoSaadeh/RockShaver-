b15519c13c269a9c34c71329d2e6c47b0238abbd
Adriano Saadeh <adrianosaadeh@gmail.com>
2024-12-08 15:25:44 -0300

    usando banco de dados

--- web/cypress/e2e/agendamento.cy.js
+++ web/cypress/e2e/agendamento.cy.js
@@ -1,6 +1,11 @@
 describe('Agendamento', () => {
     it('Deve fazer um novo agendamento', () => {
 
+        cy.dropCollection('agendamentos', { failSilently: 'true' })
+            .then(result => {
+                cy.log(result);
+            });
+
         const user = {
             nome: 'Adriano Agendamento',
             email: 'adrianoagendamento@gmail.com'
@@ -17,5 +22,24 @@ describe('Agendamento', () => {
             .parent()
             .click()
 
+        cy.contains('span', 'Serviços')
+            .should('be.visible')
+
+        cy.contains('div', 'Combo')
+            .parent()
+            .click()
+
+        cy.contains('span', 'Dias Disponíveis')
+            .should('be.visible')
+        cy.contains('span', 'Horários Disponíveis')
+            .should('be.visible')
+
+        cy.contains('.dia-semana', 10).click()
+        cy.contains('.hora-opcao', '11:00').click()
+        cy.contains('button', 'Confirmar e reservar').click()
+
+        cy.get('h3')
+            .should('be.visible')
+            .and('have.text', 'Tudo certo por aqui! Seu horário está confirmado.')
     })
 })
