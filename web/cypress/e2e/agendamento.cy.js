6eb89130c0fb4cf18b8b3d8069fc63854b29549e
Adriano Saadeh <adrianosaadeh@gmail.com>
2024-12-12 15:12:59 -0300

    fixtures applied

--- web/cypress/e2e/agendamento.cy.js
+++ web/cypress/e2e/agendamento.cy.js
@@ -1,18 +1,27 @@
+import calendario from '../fixtures/calendario.json'
+
 describe('Agendamento', () => {
-    it('Deve fazer um novo agendamento', () => {
+
+    beforeEach(function () {
+        cy.fixture('agendamentos').then((agendamentos) => {
+            this.agendamentos = agendamentos
+        })
+    })
+
+    it('Deve fazer um novo agendamento', function () {
 
         cy.dropCollection('agendamentos', { failSilently: 'true' })
             .then(result => {
                 cy.log(result);
             });
 
-        const user = {
-            nome: 'Adriano Agendamento',
-            email: 'adrianoagendamento@gmail.com'
-        }
+        cy.intercept('GET', 'http://localhost:3333/api/calendario', {
+            statusCode: 200,
+            body: calendario
+        }).as('getCalendario')
 
-        cy.startPreRegistration(user)
-        cy.verifyPreRegistered(user)
+        cy.startPreRegistration(this.agendamentos.sucesso.usuario)
+        cy.verifyPreRegistered(this.agendamentos.sucesso.usuario)
 
         cy.contains('a[href="/agendamento"]', 'Agendar um horário').click()
         cy.contains('span', 'Membros da Equipe')
@@ -25,7 +34,7 @@ describe('Agendamento', () => {
         cy.contains('span', 'Serviços')
             .should('be.visible')
 
-        cy.contains('div', 'Combo')
+        cy.contains('div', this.agendamentos.sucesso.servico.descricao)
             .parent()
             .click()
 
@@ -34,8 +43,8 @@ describe('Agendamento', () => {
         cy.contains('span', 'Horários Disponíveis')
             .should('be.visible')
 
-        cy.contains('.dia-semana', 10).click()
-        cy.contains('.hora-opcao', '11:00').click()
+        cy.contains('.dia-semana', this.agendamentos.sucesso.dia).click()
+        cy.contains('.hora-opcao', this.agendamentos.sucesso.hora).click()
         cy.contains('button', 'Confirmar e reservar').click()
 
         cy.get('h3')
