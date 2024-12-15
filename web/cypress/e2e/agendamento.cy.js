d42c9a155f5396bd398e29683d7cf970d55c9d6d
Adriano Saadeh <adrianosaadeh@gmail.com>
2024-12-15 10:15:45 -0300

    enhacements

--- web/cypress/e2e/agendamento.cy.js
+++ web/cypress/e2e/agendamento.cy.js
@@ -6,19 +6,20 @@ describe('Agendamento', () => {
         cy.fixture('agendamentos').then((agendamentos) => {
             this.agendamentos = agendamentos;
         });
-    });
-
-    it('Deve fazer um novo agendamento', function () {
-
-        cy.dropCollection('agendamentos', { failSilently: 'true' })
-            .then(result => {
-                cy.log(result);
-            });
-
         cy.intercept('GET', 'http://localhost:3333/api/calendario', {
             statusCode: 200,
             body: calendario
         }).as('getCalendario');
+    });
+
+    it('Deve fazer um novo agendamento', function () {
+
+        cy.deleteMany(
+            { emailCliente: this.agendamentos.sucesso.usuario.email },
+            { collection: 'agendamentos' }
+        ).then(result => {
+            cy.log(result);
+        });
 
         cy.startPreRegistration(this.agendamentos.sucesso.usuario);
         cy.verifyPreRegistered(this.agendamentos.sucesso.usuario);
@@ -35,21 +36,18 @@ describe('Agendamento', () => {
     })
 
 
-    it.only('Deve mostrar o slot ocupado', function () {
+    it('Deve retornar uma notificação em caso de conflito de disponibilidade', function () {
 
         const agendamentos = this.agendamentos.duplicado
 
-        cy.dropCollection('agendamentos', { failSilently: 'true' })
-            .then(result => {
-                cy.log(result);
-            });
+        cy.deleteMany(
+            { emailCliente: agendamentos.usuario.email },
+            { collection: 'agendamentos' }
+        ).then(result => {
+            cy.log(result);
+        });
 
         cy.agendamentoApi(agendamentos)
-
-        cy.intercept('GET', 'http://localhost:3333/api/calendario', {
-            statusCode: 200,
-            body: calendario
-        }).as('getCalendario');
 
         cy.startPreRegistration(agendamentos.usuario);
         cy.verifyPreRegistered(agendamentos.usuario);
@@ -64,6 +62,35 @@ describe('Agendamento', () => {
             .find('svg')
             .should('be.visible')
             .and('have.css', 'color', 'rgb(255, 255, 255)')
+    })
+
+    it('Deve mostrar o slot ocupado', function () {
+
+        const agendamentos = this.agendamentos.conflito
+
+        cy.deleteMany(
+            { emailCliente: agendamentos.usuario.email },
+            { collection: 'agendamentos' }
+        ).then(result => {
+            cy.log(result);
+        });
+
+        cy.startPreRegistration(agendamentos.usuario);
+        cy.verifyPreRegistered(agendamentos.usuario);
+
+        cy.iniciarAgendamento();
+        cy.escolherProfissional(agendamentos.profissional.nome);
+        cy.selecionarServico(agendamentos.servico.descricao);
+        cy.escolherDia(agendamentos.dia);
+        cy.escolherHorario(agendamentos.hora)
+
+        cy.agendamentoApi(agendamentos)
+
+        cy.finalizarAgendamento()
+
+        cy.get('.alert-error')
+            .should('be.visible')
+            .and('have.text', 'Já existe um agendamento para esta data e hora. Por favor, escolha outro horário.')
     })
 })
 
